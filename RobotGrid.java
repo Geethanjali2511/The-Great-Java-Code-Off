6204ea2962006c7a6a3227a42503f9eb2ce9a1ba
Geethanjali Vittal <geethanjali.vittal@version1.com>
2023-07-07 12:23:04 +0100

    just the isSafemethod implementation

--- RobotGrid.java
+++ RobotGrid.java
@@ -3,7 +3,29 @@ public class RobotGrid {
 
     public boolean isSafe(int x, int y) {
         // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+        int totalx = 0;
+        int totaly = 0;
+
+        int[] arr1 = Integer.toString(x).chars().map(c -> c - '0').toArray();
+        for (int d : arr1) {
+            System.out.print(d);
+        }
+        int[] arr2 = Integer.toString(y).chars().map(a -> a - '0').toArray();
+        for (int b : arr2) {
+            System.out.print(b);
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            totalx = totalx + arr1[i];
+        }
+        if (totalx <= 23 && totaly <= 23) {
+            return true;
+        } else {
+            return false;
+        }
+
+        // throw new UnsupportedOperationException("Not implemented yet");
+
     }
 
     public int totalSafeSquares() {
